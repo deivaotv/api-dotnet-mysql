2cc0552288496ff2353d46c0dcfc4d6e977377cc
deivaotv <xdeividsabinox@live.com>
2021-01-24 18:54:39 -0300

    iniciando com dotnet api

---
+++ Repository/IPessoaRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using ApiMySql.Model;
+
+namespace ApiMySql.Repository
+{
+    public interface IPessoaRepository
+    {
+        IEnumerable<Pessoa> GetAll();
+    }
+
+}
